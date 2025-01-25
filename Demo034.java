a498abbebd7ceb19a065057bca929119bf070228
harish8790 <harishkumar200528@gmail.com>
2025-01-25 10:28:54 +0530

    Add files via upload

---
+++ Demo034.java
@@ -0,0 +1,13 @@
+public class Demo034 {
+    public static void main(String[] args){
+        
+        int num1 = 11;
+        if(num1 % 2 == 0) {
+            System.out.println("Even number");
+        }
+        else {
+            System.out.println("odd number");
+        }
+        
+    }
+}
