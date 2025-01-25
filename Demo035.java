40a97c5ec050c4e45c9565c3d3d23aa0bd31b2a7
harish8790 <harishkumar200528@gmail.com>
2025-01-25 10:29:13 +0530

    Add files via upload

---
+++ Demo035.java
@@ -0,0 +1,13 @@
+public class Demo035{
+    public static void main(String[] args){
+        int a,b;
+        a = 10;
+        b = 20;
+        if(a > b){
+            System.out.println("a is greater");
+        }
+        else {
+            System.out.println("b is greater");
+        }
+    }
+}
