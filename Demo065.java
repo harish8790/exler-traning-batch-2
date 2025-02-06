323b3aaa2195d93e46509081b751d3ec428c88b5
harish8790 <harishkumar200528@gmail.com>
2025-02-06 09:35:00 +0530

    Add files via upload

---
+++ Demo065.java
@@ -0,0 +1,7 @@
+public class Demo065 {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+        
+        System.out.println("elemens in the array are: " + java.util.Arrays.toString(arr));
+    }
+}
